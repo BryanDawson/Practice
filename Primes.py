48434d184d63569a781c38e0edbae21a4fdc7f09
BryanDawson <free2be@fastmail.fm>
2017-11-15 17:29:54 -0800

    Updated Primes.py with a much faster prime number test

--- Primes.py
+++ Primes.py
@@ -18,6 +18,41 @@ def is_int(str_val):
     return True
 
 
+# strong probable prime
+def is_sprp(n, b=2):
+    """Returns True if n is prime.
+
+        Code borrowed from:
+        https://codegolf.stackexchange.com/a/10702
+
+        Implementation of Baillie–PSW:
+        https://en.wikipedia.org/wiki/Baillie%E2%80%93PSW_primality_test
+        This one is a bit uglier, but is vastly faster
+        Tested: Almost immediate response up to 200 digits
+    """
+
+    d = n-1
+    s = 0
+    while d&1 == 0:
+        s += 1
+        d >>= 1
+
+    x = pow(b, d, n)
+    if x == 1 or x == n-1:
+        return True
+
+    for r in range(1, s):
+        x = (x * x)%n
+        if x == 1:
+            return False
+        elif x == n-1:
+            return True
+
+    return False
+
+
+
+
 def isprime(n):
     """Returns True if n is prime.
 
@@ -71,7 +106,7 @@ def main():
             print(test, "doesn't look like a number, try again")
             continue
 
-        if isprime(int(test)):
+        if is_sprp(int(test)):
             print(test, "is prime!")
         else:
             print(test, "is not prime.")
